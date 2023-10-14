ba292568285df6a450e07d2b6f6f4b652cef7a54
giordan-zeina <144264315+giordan-zeina@users.noreply.github.com>
2023-10-13 23:45:12 -0400

    typescript version

--- main.ts
+++ main.ts
@@ -6,23 +6,23 @@
 */
 
 // variables
-let randomNumber : number = 0
-let score : number = 0
+let randomNumber: number = 0
+let score: number = 0
 randomNumber = -1
 basic.clearScreen()
 basic.showIcon(IconNames.Happy)
 
-input.onGesture(Gesture.Shake, function() {
-  randomNumber = randint(0,2)
+input.onGesture(Gesture.Shake, function () {
+  randomNumber = randint(0, 2)
   basic.clearScreen()
 
   // if randomNumber is 0
-  if (randomNumber == 0) {
+  if (randomNumber === 0) {
     basic.showIcon(IconNames.Scissors)
   }
 
   // if randomNumber is 1
-  if (randomNumber == 1) {
+  if (randomNumber === 1) {
     basic.showLeds(`
     . . . . .
     . # # # .
@@ -33,7 +33,7 @@ input.onGesture(Gesture.Shake, function() {
   }
 
   // if randomNumber is 2
-  if (randomNumber == 2) {
+  if (randomNumber === 2) {
     basic.showLeds(`
     # # # # #
     # . . . #
@@ -44,10 +44,10 @@ input.onGesture(Gesture.Shake, function() {
   }
 })
 
-input.onButtonPressed(Button.A, function() {
+input.onButtonPressed(Button.A, function () {
   score = score + 1
 })
 
-input.onButtonPressed(Button.B, function() {
+input.onButtonPressed(Button.B, function () {
   basic.showNumber(score)
 })
